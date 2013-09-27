27ebe8b6a85f8fe466baf7295d31f005ebba5a7f
Dirk Van Haerenborgh <vhdirk@gmail.com>
2013-09-27 08:33:49 +0200

    initial import

---
+++ config.h
@@ -0,0 +1,130 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* always defined to indicate that i18n is enabled */
+#define ENABLE_NLS 1
+
+/* "gettext package name" */
+#define GETTEXT_PACKAGE "gst-editor"
+
+/* path to glade ui dir */
+#define GLADEUI_UNINSTALLED_DIR "/home/dvhaeren/development/gstreamer/gsteditor/src"
+
+/* Define if debugging messages should be colorized */
+#define GST_DEBUG_COLOR 1
+
+/* Define if DEBUG statements should be compiled in */
+#define GST_DEBUG_ENABLED 1
+
+/* The application-specific data dir */
+#define GST_EDITOR_DATA_DIR "/usr/local/share/gsteditor"
+
+/* Define if INFO statements should be compiled in */
+#define GST_INFO_ENABLED 1
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* Define to 1 if you have the MacOS X function CFLocaleCopyCurrent in the
+   CoreFoundation framework. */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+
+/* Define to 1 if you have the MacOS X function CFPreferencesCopyAppValue in
+   the CoreFoundation framework. */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define if you have the iconv() function and it works. */
+/* #undef HAVE_ICONV */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if `stat' has the bug that it succeeds when given the
+   zero-length file name argument. */
+/* #undef HAVE_STAT_EMPTY_STRING_BUG */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* gettext locale dir */
+#define LOCALEDIR "/usr/local/share/locale"
+
+/* Define to 1 if `lstat' dereferences a symlink specified with a trailing
+   slash. */
+#define LSTAT_FOLLOWS_SLASHED_SYMLINK 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "gsteditor"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://gstreamer.freedesktop.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GstEditor"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GstEditor 0.10.3.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gsteditor"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.10.3.2"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.10.3.2"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
---
+++ libs/gst/common/gste-marshal.h
@@ -0,0 +1,24 @@
+
+#ifndef __gst_editor_marshal_MARSHAL_H__
+#define __gst_editor_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:POINTER (gste-marshal.list:1) */
+#define gst_editor_marshal_VOID__POINTER	g_cclosure_marshal_VOID__POINTER
+
+/* VOID:OBJECT (gste-marshal.list:2) */
+#define gst_editor_marshal_VOID__OBJECT	g_cclosure_marshal_VOID__OBJECT
+
+/* VOID:INT (gste-marshal.list:3) */
+#define gst_editor_marshal_VOID__INT	g_cclosure_marshal_VOID__INT
+
+/* VOID:VOID (gste-marshal.list:4) */
+#define gst_editor_marshal_VOID__VOID	g_cclosure_marshal_VOID__VOID
+
+G_END_DECLS
+
+#endif /* __gst_editor_marshal_MARSHAL_H__ */
+
